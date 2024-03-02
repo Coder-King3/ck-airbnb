b1cb3a1e744e19b3f6bcbbd3761b1389b531402a
Coder-King3 <2196592083@qq.com>
2024-03-02 11:33:56 +0800

    🧩fix: 更改baseUrl为代理服务器url，解决在部署到Vercel上https网页发送http请求被阻止的问题

--- src/services/request/config.js
+++ src/services/request/config.js
@@ -1,3 +1,9 @@
-export const BASE_URL = "http://codercba.com:1888/airbnb/api"
-export const TIMEOUT = 10000
+// 原接口
+// export const BASE_URL = "http://codercba.com:1888/airbnb/api"
 
+// 使用代理解决在部署到Vercel上https网页发送http请求被阻止的问题
+export const BASE_URL =
+  'https://player-mixed-services.vercel.app/airbnb-proxy-api'
+// export const BASE_URL = 'http://localhost:3068/airbnb-proxy-api'
+
+export const TIMEOUT = 10000
